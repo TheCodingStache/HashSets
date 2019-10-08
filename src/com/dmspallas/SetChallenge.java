102dad6c94035333b20a3e79298d28dc507ee670
dmspallas <dmspallas@gmail.com>
2019-10-08 03:27:55 +0300

    Reconfigured the whole project from scratch

---
+++ src/com/dmspallas/SetChallenge.java
@@ -0,0 +1,12 @@
+package com.dmspallas;
+
+import java.util.*;
+
+public class SetChallenge extends HeavenlyBody {
+
+
+    public SetChallenge(String name, double orbitalPeriod, BodyTypes bodyType) {
+        super(name, orbitalPeriod, bodyType);
+    }
+
+}
